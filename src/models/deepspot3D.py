285bb339ce5ace126971b9677ff284797234b403
Joshua Stiller <joshua.stiller@2nafish.bio>
2024-11-12 15:45:28 +0100

    DEP: Removed AWS dataset
    This is now handled with the regular dataset class, as we use regular downloading of the dataset in s3 now

---
+++ src/models/deepspot3D.py
@@ -0,0 +1,4 @@
+# deepspot3D.py
+# Description: A brief description of what this file does.
+# Author: Joshua Stiller
+# Date: 06.11.24
---
+++ src/plotting/plotting_utils.py
@@ -0,0 +1,4 @@
+# plotting_utils.py
+# Description: A brief description of what this file does.
+# Author: Joshua Stiller
+# Date: 05.11.24
