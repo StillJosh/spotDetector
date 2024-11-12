82c9d0888f30fa5c2b0205c7ba8b2b5ea082e51d
Joshua Stiller <joshua.stiller@2nafish.bio>
2024-11-12 15:53:48 +0100

    DEP: Removed AWS dataset
    This is now handled with the regular dataset class, as we use regular downloading of the dataset in s3 now

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -4,6 +4,5 @@
 # Date: 17.10.24
 
 from .deepSpotDataset import DeepSpotDataset
-from .deepSpotDatasetS3 import DeepSpotDatasetS3
 
-__all__ = ['DeepSpotDataset', 'DeepSpotDatasetS3']
+__all__ = ['DeepSpotDataset']
